b235502ca761ec094335ca42792d9b251c7ea9eb
sjakos <sjakos@users.noreply.github.com>
2017-05-12 23:42:24 -0500

    add default date setter and initialize with default

--- public/scripts/asteroids.js
+++ public/scripts/asteroids.js
@@ -1,6 +1,21 @@
+$( document ).ready( function() {
+   initialize(); 
+    });
+
 /**
- * Initialize
+ * Set up page with default dates and prepare event listeners
  */
-$(document).ready(function(){
-    setup();
-});
+function initialize() {
+    set_value_to_today("start_date");
+};
+
+/**
+ * Use to set default date value to today in form YYYY-MM-DD
+ * 
+ * @param {string} elementID    - ID to be set by function
+ */
+function set_value_to_today( elementID ) {
+    var today = new Date();
+    today = today.toISOString().slice(0,10);
+    $("#" + elementID).val(today);
+};
